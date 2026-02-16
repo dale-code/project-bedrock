e98df3f0b168b9a530f45c0c95066adf985f3ec4
Boluwadale Daramola <boluwadaledara2018@gmail.com>
2026-02-16 06:30:00 +0100

    Finalize lambda handler

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,2 +1,4 @@
 def handler(event, context):
-    print("Lambda triggered")
+    for record in event['Records']:
+        filename = record['s3']['object']['key']
+        print(f"Image received: {filename}")
